cc68baf7ea5aff97499986828197bf5b466fe5da
CaioPedras <caio.vp@gmail.com>
2022-04-20 17:39:52 -0300

    PuxarQuizzAPI+LayoutCriarQuizz

--- scripts.js
+++ scripts.js
@@ -1,3 +1,30 @@
+//Variaveis Globais
+const API = 'https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes';
+//Start
+getAPI()
+//Funções
 function toggleHidden(element){
-document.querySelector(element).classList.toggle('hidden');
+console.log(element)
+    document.querySelector(element).classList.toggle('hidden');
+}
+function getAPI(){
+    const promisse = axios.get(API)
+    promisse.then(pullQuizz)
+    promisse.catch()
+}
+function pullQuizz (success){
+    
+    let quizzInnerHTML =''
+    for(let i = 0; i < success.data.length; i++){
+       quizzInnerHTML += ` <div class="quizz">
+       <img src="${success.data[i].image}">
+       <h3> ${success.data[i].title}</h3>
+       </div>`
+    }
+    console.log(quizzInnerHTML)
+    document.querySelector('.quizzes').innerHTML=quizzInnerHTML
+}
+
+function checkSuccess (success) {
+    console.log(success)
 }
