7a4711143aea22df23059fea70f0b4a9cb9d62e7
Eduardo Pacheco <eduardooliveirapacheco131@gmail.com>
2022-04-21 10:33:07 -0300

    Validação das informações básicas da criação do quizz

--- scripts.js
+++ scripts.js
@@ -1,5 +1,27 @@
 //Variaveis Globais
 const API = 'https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes';
+let objetoQuestions = {
+		title: `Título da pergunta 1`,
+		color: "#123456",
+		answers: [
+			{
+				text: "Texto da resposta 1",
+				image: "https://http.cat/411.jpg",
+				isCorrectAnswer: true
+			},
+			{
+				text: "Texto da resposta 2",
+				image: "https://http.cat/412.jpg",
+				isCorrectAnswer: false
+			}
+		]
+}
+let objetoLevels = {
+	title: "Título do nível 1",
+	image: "https://http.cat/411.jpg",
+	text: "Descrição do nível 1",
+	minValue: 0
+}
 let objetoQuizz ={
 	title: "Título do quizz",
 	image: "https://http.cat/411.jpg",
@@ -20,38 +42,6 @@ let objetoQuizz ={
 				}
 			]
 		},
-		{
-			title: "Título da pergunta 2",
-			color: "#123456",
-			answers: [
-				{
-					text: "Texto da resposta 1",
-					image: "https://http.cat/411.jpg",
-					isCorrectAnswer: true
-				},
-				{
-					text: "Texto da resposta 2",
-					image: "https://http.cat/412.jpg",
-					isCorrectAnswer: false
-				}
-			]
-		},
-		{
-			title: "Título da pergunta 3",
-			color: "#123456",
-			answers: [
-				{
-					text: "Texto da resposta 1",
-					image: "https://http.cat/411.jpg",
-					isCorrectAnswer: true
-				},
-				{
-					text: "Texto da resposta 2",
-					image: "https://http.cat/412.jpg",
-					isCorrectAnswer: false
-				}
-			]
-		}
 	],
 	levels: [
 		{
@@ -74,7 +64,6 @@ let objetoQuizz ={
 //getAPI()
 //Funções
 function toggleHidden(element){
-console.log(element)
     document.querySelector(element).classList.toggle('hidden');
 }
 function getAPI(){
@@ -95,10 +84,30 @@ function pullQuizz (success){
         </div>
        </div>`
     }
-    console.log(quizzInnerHTML)
     document.querySelector('.quizzes').innerHTML=quizzInnerHTML
 }
 
 function checkSuccess (success) {
     console.log(success)
+}
+
+function verifyBasicInformation() {
+	let quizzTitle =  document.querySelector(".infoQuizz input:nth-child(1)").value;
+	console.log(quizzTitle.length);
+	let quizzURL = document.querySelector(".infoQuizz input:nth-child(2)").value;
+	console.log(quizzURL);
+	let quizzNumberQuestions = document.querySelector(".infoQuizz input:nth-child(3)").value;
+	console.log(quizzNumberQuestions);
+	let quizzNumberLevels = document.querySelector(".infoQuizz input:nth-child(4)").value;
+	console.log(quizzNumberLevels);
+
+	if (quizzTitle === "" || quizzTitle.length > 65 || quizzTitle.length < 20 || quizzURL.startsWith("http") === false || quizzURL === "" || quizzNumberQuestions === "" || quizzNumberQuestions < 3 || quizzNumberLevels === "" || quizzNumberLevels < 2) {
+		alert("Preencha os dados corretamente");
+	} else {
+		console.log(objetoQuizz.questions);
+		objetoQuizz = {title:`${quizzTitle}`, image: `${quizzURL}`, questions: `Array[${quizzNumberQuestions}]`, levels: `Array[${quizzNumberLevels}]`}
+		console.log(objetoQuizz);
+		toggleHidden('.section:nth-child(1)');
+		toggleHidden('.section:nth-child(2)');
+	}
 }
